4a61e09b45b3ed3e325034ae3bd7ca23c4d838fb
sepubi <160240637+sepubi@users.noreply.github.com>
2026-02-12 12:50:13 +0100

    Clase Pila

--- Torres_de_Hanoi/Pila.cs
+++ Torres_de_Hanoi/Pila.cs
@@ -4,40 +4,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System;
+using System.Collections.Generic;
+
 namespace Torres_de_Hanoi
 {
     class Pila
     {
-        public int Size { get; set; }
-        /* TODO: Elegir tipo de Top
-        public int Top { get; set; }
-        public String Top { get; set; }        
-        */
-        /* TODO: Elegir tipo de Elementos
-        public Disco[] Elementos { get; set; }
-        public List<Disco> Elementos { get; set; }
-        */
+        public int Size { get { return Elementos.Count; } } // Cantidad de discos
+        public Disco Top { get { return Elementos.Count > 0 ? Elementos[Elementos.Count - 1] : null; } } // Disco superior
+        public List<Disco> Elementos { get; set; } // Conjunto de discos
+        public string Nombre { get; set; } // Nombre de la pila
 
-        /* TODO: Implementar métodos */
-        public Pila()
+        public Pila(string nombre)
         {
-
+            Nombre = nombre;
+            Elementos = new List<Disco>();
         }
 
         public void push(Disco d)
         {
-
+            if (Top == null || Top.Valor > d.Valor)
+            {
+                Elementos.Add(d);
+            }
+            else
+            {
+                throw new InvalidOperationException("No se puede colocar un disco más grande sobre uno más pequeño.");
+            }
         }
 
         public Disco pop()
         {
-            return null;
-        }                
+            if (Elementos.Count == 0) return null;
+            Disco d = Top;
+            Elementos.RemoveAt(Elementos.Count - 1);
+            return d;
+        }
 
         public bool isEmpty()
         {
-            return true;
+            return Elementos.Count == 0;
         }
 
+        public void Mostrar()
+        {
+            Console.Write($"{Nombre}: ");
+            foreach (var disco in Elementos)
+                Console.Write(disco.Valor + " ");
+            Console.WriteLine();
+        }
     }
 }
